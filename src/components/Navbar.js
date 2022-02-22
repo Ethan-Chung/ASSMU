56d8797c465519d544f28aa68851397b79000aae
Ethan <Ethan.k.chung@outlook.com>
2022-02-22 14:39:54 -0800

    Changed Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,37 @@
-import { Link } from 'react-router-dom';
+import { Link} from 'react-router-dom';
 import assmulogo from '../images/assmulogo.jpg';
 import React from "react";
+import { Container, Navbar, Nav, NavDropdown, Button } from 'react-bootstrap';
 
-const Navbar = () => {
+const Navbars = () => {
     return (
-        <nav className="navbar">
+        
+        <div>            
+        <Navbar bg="light" expand="lg">
+        <Container>
+            <Navbar.Brand as={Link} to={"/"} className="navbar">
+                <img src={assmulogo} id="logo" className="navbar-image"/>
+                ASSMU
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav navbar">
+            <Nav className="ms-auto navbar">
+                <Nav.Link as={Link} to={"/clubs"}>Clubs</Nav.Link>
+                <Nav.Link as={Link} to={"/events"}>Events</Nav.Link>
+                <Nav.Link as={Link} to={"/assmu"}>ASSMU</Nav.Link>
+                <Nav.Link as={Link} to={"/calendar"}>Calendar</Nav.Link>
+                <Nav.Link as={Link} to={"/resources"}>Resources</Nav.Link>
+                <Nav.Link as={Link} to={"/contact"}>Contact</Nav.Link>          
+            </Nav>
+            </Navbar.Collapse>
+        </Container>
+        </Navbar>
+        </div>
+       
+        
+        
+        
+        /*<nav className="navbar">
             <div className="links">
                 <Link to="/clubs">Clubs</Link>
                 <Link to="/events">Events</Link>
@@ -15,8 +42,8 @@ const Navbar = () => {
                 <Link to="/contact">Contact</Link>
 
             </div>
-        </nav>
+        </nav>*/
     );
 }
  
-export default Navbar;
+export default Navbars;

7a8322668ca32fdc0bbb44281d8926c24cc22168
Ethan <Ethan.k.chung@outlook.com>
2022-02-22 14:41:34 -0800

    removed unnessary code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -27,22 +27,6 @@ const Navbars = () => {
         </Container>
         </Navbar>
         </div>
-       
-        
-        
-        
-        /*<nav className="navbar">
-            <div className="links">
-                <Link to="/clubs">Clubs</Link>
-                <Link to="/events">Events</Link>
-                <Link to="/assmu">ASSMU</Link>
-                <Link to="/"> <img src={assmulogo} id = "logo" alt = "ASSMU logo"/> </Link>
-                <Link to="/calendar">Calendar</Link>
-                <Link to="/resources">Resources </Link>
-                <Link to="/contact">Contact</Link>
-
-            </div>
-        </nav>*/
     );
 }
  

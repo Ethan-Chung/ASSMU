f5c5ef16c351d4712e59ce1b6fc1ba16d2e42195
JuicetinRe <lejustin24@gmail.com>
2022-03-22 15:49:43 -0700

    Added styling and formatting to Assmu.js

--- src/components/Assmu.js
+++ src/components/Assmu.js
@@ -1,7 +1,38 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react/cjs/react.development';
+import React from "react";
+
 const Assmu = () => {
+
+    const [info, setInfo] = useState('');
+    const url = 'http://localhost:3001'
+  
+    useEffect(() => {
+      axios.get(url, {
+        'Content-Type':'application/json'     
+      }).then(response => {
+          setInfo(response.data)
+          console.log(response.data)
+        })
+    }, [])
+
+    const arr = Object.values(info)
+
     return ( 
-        <div>
-            <h1>Dis da officerz</h1>
+        <div className="allOfficers">
+            {
+            //img src will be club logo
+            //club.title is each club name
+            arr.map((officer, index) => {
+              return (
+                <div className = "officers" key = {index}>
+                    <img src={`${officer.metadata.hero.imgix_url}`} className = "officerImage" alt='clubimage'></img>
+                    <div className = "officerName"> {officer.title} </div>
+                    <div className="officerDescription"> {officer.content}</div>
+                </div>
+              )
+            })
+          }
         </div>
      );
 }

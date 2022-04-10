a92404e9934b84a858589faec725d3ea90ed5aed
shawn.mathews <shawn.mathews@stmartin.edu>
2022-04-10 11:38:02 -0700

    pulling data from backend

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Google_Api_Key, Client_Id } from '../config.js';
 import { getClubTypes } from '../services/ClubTypesService';
+import axios from 'axios';
 import { paginate } from '../utils/paginate.js';
 import ListGroup from './reusable/ListGroup.js';
 import Pagination from './reusable/Pagination.js';
@@ -16,6 +17,8 @@ const Events = () => {
 	const [searchQuery, setSearchQuery] = useState('');
 	const [pageSize, setPageSize] = useState(3);
 	const [currentPage, setCurrentPage] = useState(1);
+	const [info, setInfo] = useState('');
+	const url = 'http://localhost:3002';
 
 	const options = {
 		weekday: 'long',
@@ -29,31 +32,21 @@ const Events = () => {
 		minute: '2-digit',
 	};
 
-	const SCOPES = 'https://www.googleapis.com/auth/calendar.events.readonly'; // permissions
-
 	useEffect(() => {
+		axios
+			.get(url, {
+				'Content-Type': 'application/json',
+			})
+			.then((response) => {
+				setInfo(response.data);
+				console.log(response.data);
+			});
 		// clone existing array and add new clubType item to it
 		const clubTypes = [{ _id: '', name: 'All Clubs' }, ...getClubTypes()];
 
 		// setting state of clubTypes available
 		setClubTypes(clubTypes);
-
-		// when the component is mounted into the DOM
-		const script = document.createElement('script');
-		script.async = true;
-		script.defer = true;
-		script.src = 'https://apis.google.com/js/api.js';
-
-		document.body.appendChild(script);
-
-		script.addEventListener('load', () => {
-			if (window.gapi) handleClientLoad();
-		});
 	}, []);
-
-	const handleClientLoad = () => {
-		window.gapi.load('client:auth2', initClient);
-	};
 
 	const handleClubTypeSelect = (clubType) => {
 		// set state of the clubType we clicked on
@@ -71,53 +64,6 @@ const Events = () => {
 		setSearchQuery(query);
 		setSelectedClubType(null);
 		setCurrentPage(1);
-	};
-
-	const openSignInPopup = () => {
-		window.gapi.auth2.authorize(
-			{ client_id: Client_Id, scope: SCOPES },
-			(res) => {
-				if (res) {
-					if (res.access_token)
-						localStorage.setItem('access_token', res.access_token);
-
-					// Load calendar events after authentication
-					window.gapi.client.load('calendar', 'v3', listUpcomingEvents);
-				}
-			}
-		);
-	};
-
-	const initClient = () => {
-		if (!localStorage.getItem('access_token')) {
-			openSignInPopup();
-		} else {
-			// Get events if access token is found without sign in popup
-			fetch(
-				`https://www.googleapis.com/calendar/v3/calendars/primary/events?key=${Google_Api_Key}&orderBy=startTime&singleEvents=true`,
-				{
-					headers: {
-						Authorization: `Bearer ${localStorage.getItem('access_token')}`,
-					},
-				}
-			)
-				.then((res) => {
-					// Check if unauthorized status code is return open sign in popup
-					if (res.status !== 401) {
-						return res.json();
-					} else {
-						localStorage.removeItem('access_token');
-
-						openSignInPopup();
-					}
-				})
-				.then((data) => {
-					if (data?.items) {
-						setEvents(formatEvents(data.items));
-						console.log(data.items);
-					}
-				});
-		}
 	};
 
 	// list events on Google Calendar
